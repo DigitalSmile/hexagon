06b3dd05fb8bbecec747240dc58c2c049868fc63
Nick Gritsenko <digitalsmile.dig@gmail.com>
2022-10-19 17:38:37 +0300

    Fix code smells

--- src/main/java/org/digitalsmile/hexgrid/shapes/ShapeBuilderFactory.java
+++ src/main/java/org/digitalsmile/hexgrid/shapes/ShapeBuilderFactory.java
@@ -5,15 +5,16 @@ import org.digitalsmile.hexgrid.shapes.hexagonal.HexagonalShapeBuilder;
 import org.digitalsmile.hexgrid.shapes.rectangle.RectangleShape;
 import org.digitalsmile.hexgrid.shapes.rectangle.RectangleShapeBuilder;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class ShapeBuilderFactory {
 
-    private static final Map<Class<? extends Shape>, ShapeBuilder<? extends Shape>> map = new HashMap<>() {{
-        put(RectangleShape.class, new RectangleShapeBuilder());
-        put(HexagonalShape.class, new HexagonalShapeBuilder());
-    }};
+    private static final Map<Class<? extends Shape>, ShapeBuilder<? extends Shape>> map = Map.of(
+            RectangleShape.class, new RectangleShapeBuilder(),
+            HexagonalShape.class, new HexagonalShapeBuilder()
+    );
+
+    private ShapeBuilderFactory(){}
 
     @SuppressWarnings("unchecked")
     public static <S extends Shape> ShapeBuilder<S> createShapeCreator(Class<? extends Shape> clazz) {
